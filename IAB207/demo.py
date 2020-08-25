9e67e8b90ba01e50a937762fd04f8349d2217851
Jason Leong <n8329711@qut.edu.au>
2020-08-25 10:11:13 +1000

    Create demo.py

---
+++ IAB207/demo.py
@@ -0,0 +1,23 @@
+print("Hello World")
+
+x = "hello"
+y = "world"
+print(x+y)
+
+txt = x.replace('h', 'power')
+print(txt)
+
+# list
+s = [1, 2, 3, 4]
+t = s
+s.append(5)
+print(t)
+
+# dictionaries for name value pairs
+
+rooms = {'double': 240, 'standard': 100}
+
+print(rooms)
+# check if a key exists
+check = 'double' in rooms
+print(check)
